cd04f49e104c01cecafad5aab175c5783420914a
Renato Luiz Carvalho <rcarva5.example@git.vmcommerce.intra>
2015-07-31 14:59:54 -0300

    Project Files Update

---
+++ RSSSolution/WebSiteRSS/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/custom.css" rel="stylesheet" type="text/css" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container backGround">
+        <br />
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">RSS Tec Mundo</a>
+            </div>
+        </nav>
+        <br />
+        <asp:Repeater ID="rptRSS" runat="server">
+            <ItemTemplate>
+                <div>
+                    <h3>
+                        <asp:Label Text='<%# Eval("Title") %>' runat="server" ID="lblTitle"></asp:Label></h3>
+                    <a href='<%# Eval("Link") %>'>
+                        <%# Eval("Link") %></a>
+                    <p>
+                        <%# Eval("Description") %>
+                    </p>
+                </div>
+                <br />
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+    </form>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ RSSSolution/WebSiteRSS/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Reader;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var objRSS = new RSS();
+        objRSS.ExecuteUrlRead();
+
+        System.Xml.XmlNodeList rssItems = objRSS.Document.SelectNodes("rss/channel/item");
+
+        string title = "";
+        string link = "";
+        string description = "";
+        objRSS.LstRSS = new List<RSSItem>();
+
+        for (int i = 0; i < rssItems.Count; i++)
+        {
+            System.Xml.XmlNode rssDetail;
+
+            rssDetail = rssItems.Item(i).SelectSingleNode("title");
+            if (rssDetail != null)
+            {
+                title = rssDetail.InnerText;
+            }
+            else
+            {
+                title = "";
+            }
+
+            rssDetail = rssItems.Item(i).SelectSingleNode("link");
+            if (rssDetail != null)
+            {
+                link = rssDetail.InnerText;
+            }
+            else
+            {
+                link = "";
+            }
+
+            rssDetail = rssItems.Item(i).SelectSingleNode("description");
+            if (rssDetail != null)
+            {
+                description = rssDetail.InnerText;
+            }
+            else
+            {
+                description = "";
+            }
+
+            objRSS.LstRSS.Add(new RSSItem { Title = title, Description = description, Link = link });
+        }
+
+        rptRSS.DataSource = objRSS.LstRSS;
+        rptRSS.DataBind();
+    }
+}
